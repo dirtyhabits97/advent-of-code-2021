8f72dcc6cfca0bd32c4479db4dbed9269a08b03d
dirtyhabits97 <gonzalorehu@gmail.com>
2021-12-04 13:01:20 -0500

    Added game logic.

--- day04/puzzle.py
+++ day04/puzzle.py
@@ -1,4 +1,44 @@
 
+def bingo(nums, boards):
+    for num in nums:
+
+        for board in boards:
+            mark(num, board)
+
+            if check_if_won(board):
+                return score(board) * num
+
+    return -1
+
+
+def score(board):
+    rows, cols = len(board), len(board[0])
+    ans = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if not board[row][col][1]:
+                ans += board[row][col][0]
+
+    return ans
+
+
+def mark(num, board):
+    """
+    Marks a number in the board if it exists.
+
+    This could be optimized if we keep track of the indices
+    in a dictionary (O(1)).
+    """
+
+    rows, cols = len(board), len(board[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if num == board[row][col]:
+                board[row][col][1] = True
+
+
 def check_if_won(board):
     """
     Checks if the board has a full row or column.
@@ -14,15 +54,17 @@ def check_if_won(board):
     rows, cols = len(board), len(board[0])
 
     # 1. check rows
-    for row in rows:
-        for col in cols:
+    for row in range(rows):
+        for col in range(cols):
             if not board[row][col][1]:
                 break
         return True
 
     # 2. check cols
-    for col in cols:
-        for row in rows:
+    for col in range(cols):
+        for row in range(rows):
             if not board[row][col][1]:
                 break
         return True
+
+    return False
