06bf629298109627eec9a30bb78651f4e51de05e
dirtyhabits97 <gonzalorehu@gmail.com>
2021-12-06 09:59:21 -0500

    Day 6 puzzle 1.

---
+++ day06/puzzle.py
@@ -0,0 +1,23 @@
+# This looks like a BFS problem where every parent is passed to the next level
+
+def bfs(nums, days):
+    for _ in range(days):
+        for idx in range(len(nums)):
+
+            if not nums[idx]:
+                nums.append(8)
+                nums[idx] = 6
+            else:
+                nums[idx] -= 1
+
+    return len(nums)
+
+
+def main():
+    f = open("input.txt", "r")
+    input = list(map(int, f.readlines()[0].split(",")))
+    print(bfs(input, 80))
+
+
+if __name__ == '__main__':
+    main()
