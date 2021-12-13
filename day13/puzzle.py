c727cc277e24611467633cd228b9092bc59fbd1b
dirtyhabits97 <gonzalorehu@gmail.com>
2021-12-13 11:12:49 -0500

    Day 13 puzzle 1.

--- day13/puzzle.py
+++ day13/puzzle.py
@@ -6,17 +6,52 @@ def main():
     fold(matrix, folds)
     return count_folds(matrix)
 
+
 def count_folds(matrix):
     ans = 0
     for row in matrix:
         for char in row:
             ans += 1 if char == '#' else 0
-        print(row)
     return ans
 
 
 def fold(matrix, folds):
-    pass
+    for axis, value in folds:
+        if axis == 'x':
+            fold_x(matrix, value)
+        else:
+            fold_y(matrix, value)
+        print("fold axis: %s, value: %d, # of folds: %d" % (axis, value, count_folds(matrix)))
+
+def fold_x(matrix, value):
+    for row in range(len(matrix)):
+        lhs = r = value
+        lend, rend = 0, len(matrix[0]) - 1
+
+        while lhs >= lend and r <= rend:
+            if matrix[row][r] == '#':
+                matrix[row][lhs] = '#'
+            r += 1
+            lhs -= 1
+
+        for col in range(value + 1, rend + 1):
+            matrix[row][col] = '.'
+
+
+def fold_y(matrix, value):
+    top = bot = value
+    tend, bend = 0, len(matrix) - 1
+
+    while top >= tend and bot <= bend:
+        for col in range(len(matrix[top])):
+            if matrix[bot][col] == '#':
+                matrix[top][col] = '#'
+        top -= 1
+        bot += 1
+
+    for row in range(value + 1, bend + 1):
+        for col in range(len(matrix[row])):
+            matrix[row][col] = '.'
 
 
 def fill(matrix, coordinates):
@@ -27,6 +62,7 @@ def fill(matrix, coordinates):
 def build_matrix(coordinates):
     rows = max(coordinates, key=lambda c: c[1])[1] + 1
     cols = max(coordinates, key=lambda c: c[0])[0] + 1
+    print("rows: %d, cols: %d" % (rows, cols))
     return [['.' for _ in range(cols)] for _ in range(rows)]
 
 
